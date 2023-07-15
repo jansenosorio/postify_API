0d17b2c5c96e039e788d82979e300ed30d389051
jansenosorio <dev.osorio93@gmail.com>
2023-07-15 10:13:17 -0300

    ✨chore: concluded prisma implementation and db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { SigninModule } from './signin/signin.module';
 import { PublicationModule } from './publication/publication.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, SigninModule, PublicationModule],
+  imports: [UserModule, SigninModule, PublicationModule, PrismaModule],
   controllers: [],
   providers: [],
 })
