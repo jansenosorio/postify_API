de6e7878b979331fed83b7b6daa08432b377f69d
jansenosorio <dev.osorio93@gmail.com>
2023-07-15 09:59:25 -0300

    ✨feat: created db models at Prisma

--- src/publication/publications/publications.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { PublicationsService } from './publications.service';
-
-@Controller('publications')
-export class PublicationsController {
-  constructor(private readonly publicationsService: PublicationsService) {}
-}
