e8a8a3293004dae55c494ab4723780ba600c8ec7
jansenosorio <dev.osorio93@gmail.com>
2023-07-15 10:45:24 -0300

    ✨chore: created Auth User DTO, includes Global Pipes on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { UserModule } from './user/user.module';
 import { SigninModule } from './signin/signin.module';
 import { PublicationModule } from './publications/publication.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, SigninModule, PublicationModule, PrismaModule],
+  imports: [
+    UserModule,
+    SigninModule,
+    PublicationModule,
+    PrismaModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
