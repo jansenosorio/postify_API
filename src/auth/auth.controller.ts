86741a07bb54c73e76c15c2c8a8ae850cfa92bc7
jansenosorio <dev.osorio93@gmail.com>
2023-07-15 22:41:40 -0300

    ✨feat: created POST signup route, with bussiness rules

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthSignupDTO } from './dto/auth.signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('user')
+  signup(@Body() body: AuthSignupDTO) {
+    return this.authService.signup(body);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { AuthSignupDTO } from './dto/auth.signup.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(body: AuthSignupDTO) {
+    const hashPassword = bcrypt.hashSync(body.password, 10);
+
+    const isEmailExists = await this.prisma.user.findUnique({
+      where: {
+        userEmail: body.email,
+      },
+    });
+
+    if (isEmailExists)
+      throw new HttpException('Intern Conflict', HttpStatus.CONFLICT);
+
+    await this.prisma.user.create({
+      data: {
+        userName: body.name,
+        userEmail: body.email,
+        userAvatar: body.avatar,
+        userPassword: hashPassword,
+      },
+    });
+  }
+}
