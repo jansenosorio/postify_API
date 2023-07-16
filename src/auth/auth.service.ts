483920a9999f599164e5c0d318b4c88aadb76d36
jansenosorio <dev.osorio93@gmail.com>
2023-07-16 16:48:20 -0300

    ✨feat: created POST sign with generate Token with JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,17 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { AuthSignupDTO } from './dto/auth.signup.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { AuthSignDTO } from './dto/auth.signin.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
   async signup(body: AuthSignupDTO) {
     const hashPassword = bcrypt.hashSync(body.password, 10);
@@ -27,5 +34,58 @@ export class AuthService {
         userPassword: hashPassword,
       },
     });
+  }
+
+  async login(body: AuthSignDTO) {
+    const email = body.email;
+    const password = body.password;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        userEmail: email,
+      },
+    });
+
+    if (!user)
+      throw new HttpException(
+        {
+          status: HttpStatus.UNAUTHORIZED,
+          message: 'User or password are incorret',
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+
+    const passwordMatches = await bcrypt.compareSync(
+      password,
+      user.userPassword,
+    );
+
+    if (!passwordMatches)
+      throw new HttpException(
+        {
+          status: HttpStatus.UNAUTHORIZED,
+          message: 'User or password are incorret',
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+
+    return this.createToken(body);
+  }
+
+  createToken(body: AuthSignDTO) {
+    const secret = this.configService.get('JWT_SECRET_KEY');
+
+    const token = this.jwtService.sign(
+      {
+        email: body.email,
+        password: body.password,
+      },
+      {
+        expiresIn: '60min',
+        secret: secret,
+      },
+    );
+
+    return { access_token: token };
   }
 }
